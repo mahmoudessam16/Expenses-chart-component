efba8d55dc007afcb155c24096c8339a2ee9303f
mahmoudessam16 <mahmoudemourad29@gmail.com>
2023-08-21 15:32:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// Select all elements
+let allDays = document.querySelectorAll(".day");
+let allProgress = document.querySelectorAll(".progress");
+
+// Extract data from JSON File
+fetch("data.json")
+.then(res => res.json())
+.then(data => {
+    let days = [];
+    let progresses = [];
+    for (let i = 0; i < data.length; i++) {
+        days[i] = data[i].day;
+        progresses[i] = data[i].amount;
+    }
+
+    let [amount1, amount2, amount3, amount4, amount5, amount6, amount7] = progresses;
+    let [mon, tue, wed, thu, fri, sat, sun] = days;
+    let [day1, day2, day3, day4, day5, day6, day7] = allDays;
+    let [prog1, prog2, prog3, prog4, prog5, prog6, prog7] = allProgress;
+    day1.textContent = mon;
+    day2.textContent = tue;
+    day3.textContent = wed;
+    day4.textContent = thu;
+    day5.textContent = fri;
+    day6.textContent = sat;
+    day7.textContent = sun;
+
+    prog1.style.height = `${amount1}px`;
+    prog2.style.height = `${amount2}px`;
+    prog3.style.height = `${amount3}px`;
+    prog4.style.height = `${amount4}px`;
+    prog5.style.height = `${amount5}px`;
+    prog6.style.height = `${amount6}px`;
+    prog7.style.height = `${amount7}px`;
+
+    allProgress.forEach((progress) => {
+        progress.addEventListener('mouseover', function () {
+            console.log(progress.clientHeight)
+        })
+    })
+});
+
